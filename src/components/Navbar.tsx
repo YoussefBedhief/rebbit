1231502746815b615445cc484265fb0d4a9b38e8
youssefBedhief <youssefbedhief11@gmail.com>
2023-07-14 01:25:03 +0100

    Adding SearchBar component and api logic

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { Icons } from "./Icons"
 import { buttonVariants } from "./ui/Button"
 import { getAuthSession } from "@/lib/auth"
 import UserAccountNav from "./UserAccountNav"
+import SearchBar from "./SearchBar"
 
 const Navbar = async () => {
   const session = await getAuthSession()
@@ -18,6 +19,7 @@ const Navbar = async () => {
         </Link>
 
         {/* SearchBar */}
+        <SearchBar />
 
         {/* Auth Button */}
         {session?.user ? (
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,84 @@
+"use client"
+
+import { useQuery } from "@tanstack/react-query"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "./ui/Command"
+import { useCallback, useState } from "react"
+import axios from "axios"
+import { Prisma, Subrebbit } from "@prisma/client"
+import { useRouter } from "next/navigation"
+import { Users } from "lucide-react"
+import debounce from "lodash.debounce"
+
+const SearchBar = () => {
+  const [input, setInput] = useState<string>("")
+  const router = useRouter()
+  const {
+    data: queryResults,
+    refetch,
+    isFetched,
+    isFetching,
+  } = useQuery({
+    queryFn: async () => {
+      if (!input) return []
+      const { data } = await axios.get(`/api/search?q=${input}`)
+      return data as (Subrebbit & {
+        count: Prisma.SubrebbitCountOutputType
+      })[]
+    },
+    queryKey: ["search-query"],
+    enabled: false,
+  })
+
+  const request = debounce(async () => {
+    await refetch()
+  })
+
+  const debounceRequest = useCallback(() => {
+    request()
+  }, [])
+
+  return (
+    <Command className="relative rounded-lg border max-w-lg z-50 overflow-visible">
+      <CommandInput
+        className="outline-none border-none ring-0 focus:outline-none focus:border-none "
+        placeholder="search for community ..."
+        value={input}
+        onValueChange={(text) => {
+          setInput(text)
+          debounceRequest()
+        }}
+      />
+      {input.length > 0 && (
+        <CommandList className="absolute bg-white top-full inset-x-0 shadow rounded-b-md">
+          {isFetched && <CommandEmpty>No results found.</CommandEmpty>}
+          {(queryResults?.length ?? 0) > 0 ? (
+            <CommandGroup heading="Communities">
+              {queryResults?.map((subreddit) => (
+                <CommandItem
+                  onSelect={(e) => {
+                    router.push(`/r/${e}`)
+                    router.refresh()
+                  }}
+                  key={subreddit.id}
+                  value={subreddit.name}
+                >
+                  <Users className="mr-2 h-4 w-4" />
+                  <a href={`/r/${subreddit.name}`}>r/{subreddit.name}</a>
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          ) : null}
+        </CommandList>
+      )}
+    </Command>
+  )
+}
+
+export default SearchBar
